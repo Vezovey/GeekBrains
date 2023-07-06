bc8c1ba4049e12a093f885e318ec050efd043b66
Vadim <nezhevets.vadim@gmail.com>
2023-07-06 21:46:09 +0300

    Task recorded

--- 3.Introduction to programming languages/homeWork/homeWork1/task1/TASK1.cs
+++ 3.Introduction to programming languages/homeWork/homeWork1/task1/TASK1.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Input first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if(firstNumber < secondNumber)
+{
+    Console.WriteLine($"{secondNumber} more than {firstNumber}");;
+}
+else
+{
+    
+    if(firstNumber == secondNumber)
+    {
+        Console.WriteLine($"{firstNumber} equal {secondNumber}");
+    }
+    else
+    {
+        Console.WriteLine($"{firstNumber} more than {secondNumber}");
+    }
+}
