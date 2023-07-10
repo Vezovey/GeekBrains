10a09c72f78ed095c31b83438b4ae99cbc91a6db
Vadim <nezhevets.vadim@gmail.com>
2023-07-10 16:25:21 +0300

    Write correct TASK3.cs

---
+++ 3.Introduction to programming languages/homeWork/homeWork1/task3/TASK3.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int divNumber = number % 2;
+if (divNumber == 0)
+{
+    Console.WriteLine($"Yes. {divNumber} an even number");
+}
+else
+{
+    Console.WriteLine($"No. {divNumber} not an even number");
+}
+
