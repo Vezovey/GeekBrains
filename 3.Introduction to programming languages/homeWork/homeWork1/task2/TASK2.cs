8056303c50012463e682e7a7958e1f3e58957de7
Vadim <nezhevets.vadim@gmail.com>
2023-07-10 15:57:58 +0300

    Write correct task2

---
+++ 3.Introduction to programming languages/homeWork/homeWork1/task2/TASK2.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("enter the first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the third number: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+if (secondNumber >= max) max = secondNumber;
+if (thirdNumber >= max) max = thirdNumber;
+{
+    Console.WriteLine($"This is the biggest number {max}");
+}
