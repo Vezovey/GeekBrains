624c81b46ad340db18c631e24fd7d4dfcae11824
Vadim <nezhevets.vadim@gmail.com>
2023-07-11 00:02:53 +0300

    Write correсе code to homework 2 TASK1

---
+++ 3.Introduction to programming languages/homeWork/homeWork2/TASK1/TASK1.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write your number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+if(user_number >= 100 && user_number < 1000)
+{
+  int dec = user_number % 100;
+   int ed = user_number % 10;
+   int result = (dec - ed) / 10;
+Console.WriteLine($"{result}");
+}
+else 
+Console.WriteLine("Your number is NOT correct");
