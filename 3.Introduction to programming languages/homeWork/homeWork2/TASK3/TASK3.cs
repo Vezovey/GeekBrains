d7aa15665b986c0b09c95257c0a379a839b77284
Vadim <nezhevets.vadim@gmail.com>
2023-07-14 02:02:43 +0300

    Write correct TASK3 to homework2

---
+++ 3.Introduction to programming languages/homeWork/homeWork2/TASK3/TASK3.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Choose a day of the week: ");
+int number_week = Convert.ToInt32(Console.ReadLine());
+
+if (number_week > 7)
+Console.WriteLine("Selected number is more than days per week ");
+
+if (number_week < 6)
+Console.WriteLine(" Will have to work ");
+
+if (number_week >5 && number_week <= 7)
+Console.WriteLine(" Day off ");
