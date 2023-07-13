91b9c49c99de9238c12eb034779126f419ea3227
Vadim <nezhevets.vadim@gmail.com>
2023-07-14 01:39:59 +0300

    Write correct TASK2 homework 2

---
+++ 3.Introduction to programming languages/homeWork/homeWork2/TASK2/TASK2.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Write your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+   
+    if ( number < 100 )
+    Console.WriteLine("Your number is not correct");    
+    
+    int makeThreeNumber (int number)
+    {
+    while (number > 999)    
+    {
+    number /= 10;
+    }
+    return number % 10;
+    }
+    
+    Console.WriteLine($"You num is:" +  makeThreeNumber(number));  
