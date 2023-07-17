92cb685fa929ef44ead6d9cf3a4904d936f17138
Vadim <nezhevets.vadim@gmail.com>
2023-07-17 20:54:05 +0300

    write correct task1.cs on homework3

---
+++ 3.Introduction to programming languages/homeWork/homeWork3/TASK1/TASK1.cs
@@ -0,0 +1,22 @@
+﻿void paligram(int numv)
+{
+    int ed = numv % 10;
+    int fed = numv / 10000;
+if (ed == fed)
+    {
+        ed = numv % 100 / 10;
+        fed = numv / 1000 % 10;
+    }
+    if (ed == fed)
+        {
+        Console.WriteLine("Your number is paligram");
+        }
+else
+    {
+    Console.WriteLine("Your number is not paligram");
+    }
+}
+Console.WriteLine("Write your number: ");
+int numw = Convert.ToInt32(Console.ReadLine());
+
+paligram(numw);
