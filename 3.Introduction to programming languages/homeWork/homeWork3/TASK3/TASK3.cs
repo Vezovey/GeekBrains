9d48b5152a4f6455b3ea8ca0c3b24330a0d9710e
Vadim <nezhevets.vadim@gmail.com>
2023-07-17 23:13:57 +0300

    Write correct TASK3.cs on homework3

---
+++ 3.Introduction to programming languages/homeWork/homeWork3/TASK3/TASK3.cs
@@ -0,0 +1,14 @@
+﻿void functionCoub(int number)
+{
+for (int temp = 1; temp <= number; temp++)
+{
+    double num = Math.Pow(temp, 3);
+    Console.Write( num + " " );
+}
+}
+
+Console.WriteLine("Input your number:");
+int num = Convert.ToInt32(Console.ReadLine());
+functionCoub(num);
+
+
