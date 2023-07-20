436e9d75de0cf2961d6c274e5d38b51d74e02464
Vadim <nezhevets.vadim@gmail.com>
2023-07-20 04:23:24 +0300

    Write correct TASK3 on homework4

---
+++ 3.Introduction to programming languages/homeWork/homeWork4/TASK3/TASK3.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] GetRandomArray (int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(0, 99);
+
+    }
+    return result;
+}
+
+void PrintArray(int[] arrayToPrint)
+{
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+       Console.Write(arrayToPrint[i] + " ");
+    }
+}
+
+int[] RandomArray = GetRandomArray(8);
+PrintArray(RandomArray);
