44fbfeb1f28f8dda2570cb15f6ca63366df54dfb
Vadim <nezhevets.vadim@gmail.com>
2023-07-20 03:23:56 +0300

    Write correct to TASK1.cs

---
+++ 3.Introduction to programming languages/homeWork/homeWork4/TASK1/TASK1.cs
@@ -0,0 +1,19 @@
+﻿int NaturalNumber (int from, int to)
+{
+   int result = 1;
+   int y = 0;
+
+    for (int i = from; y<to; y++)
+    {
+       result = result * i;
+    }
+     return result;
+}
+
+
+
+Console.WriteLine("Input your number A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your number B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Your natural number {numberA} on {numberB} = {NaturalNumber(numberA, numberB)}");
