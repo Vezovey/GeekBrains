c4f2371f11d3e5c559b5591e90a99d8bf4564ce1
Vadim <nezhevets.vadim@gmail.com>
2023-07-23 17:48:21 +0300

    Write correct TASK2 on homework5

---
+++ 3.Introduction to programming languages/homeWork/homeWork5/TASK2/TASK2.cs
@@ -0,0 +1,43 @@
+﻿//Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+int [] makeArray (int userSize)
+{
+    int [] array = new int [userSize];
+    Random threeNum = new Random();
+    for(int i = 0; i < userSize; i++)
+    {
+        array [i] = threeNum.Next(1, 100);
+    }
+    return array;
+}
+
+void printArray (int [] printArr)
+{
+    for(int i = 0; i < printArr.Length; i ++)
+    {
+        Console.Write(printArr[i] + " ");
+    }
+    Console.WriteLine(" ");
+}
+
+int sumSecondNumber (int [] sumNumber)
+{
+    int result = 0;
+    for(int i = 0; i < sumNumber.Length; i++)
+    {
+        if(i % 2 == 0)
+        {
+            result = result + sumNumber[i];
+        }
+    }    
+    return result;
+}
+
+
+Console.WriteLine("input your number: ");
+int userSize = Convert.ToInt32(Console.ReadLine());
+
+int [] readyArray = makeArray(userSize);
+printArray(readyArray);
+int itog = sumSecondNumber(readyArray);
+Console.WriteLine($"Sum {itog} ");
